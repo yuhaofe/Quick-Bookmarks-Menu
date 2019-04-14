cfb42b3e856339eef59fd75793c0f0bc21bfbf58
Yuhao Feng <git@yuhaofe.com>
2019-04-15 05:35:10 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Quick-Bookmarks-Menu
+# Quick Bookmarks Menu
 A chrome extension let you access bookmarks quickly.
+
+[![Install from Chrome Web Store](https://developer.chrome.com/webstore/images/ChromeWebStore_BadgeWBorder_v2_206x58.png)](https://chrome.google.com/webstore/detail/quick-bookmarks-menu/fkemipdcgbeknabedhecepcebhlnlhbf)
+
+# License
+Codes in this repository are licensed under MIT license.
+
+Icons under 'icons' folder are licensed under [CC BY-NC 4.0 License](http://creativecommons.org/licenses/by-nc/4.0/). ![Creative Commons License](https://i.creativecommons.org/l/by-nc/4.0/80x15.png)
+
+Icons by [Font Awesome](https://fontawesome.com) under 'res/font-awesome/' folder are licensed under [CC BY 4.0 License](https://creativecommons.org/licenses/by/4.0/). ![Creative Commons License](https://i.creativecommons.org/l/by/4.0/80x15.png)
---
+++ src/qbm.js
@@ -0,0 +1,105 @@
+function bmShow(item){
+    item.classList.remove('bm-hide');
+    item.classList.add('bm-show');
+}
+
+function bmHide(item){
+    item.classList.remove('bm-show');
+    item.classList.add('bm-hide');
+}
+
+function createBmItem(title, href, icon, id, parentId){
+    var bmItem = document.createElement('div');
+    bmItem.classList.add('bm-item');
+    var bmIcon = document.createElement('img');
+    bmIcon.src = icon;
+    var bmLink = document.createElement('a');
+    bmLink.innerText = title;
+    bmLink.href = href;
+
+    if (id){
+        bmItem.onclick = function(){
+            loadFolder(id, parentId);
+        };
+    }else{
+        bmLink.target = '_blank';
+    }
+
+    bmItem.appendChild(bmIcon);
+    bmItem.appendChild(bmLink);
+    return bmItem;
+}
+
+function loadFolder(id, parentId){
+    if (!id){
+        return;
+    }
+    var bmTree;
+    var bmTrees = document.querySelectorAll('.bm-tree');
+    bmTrees.forEach(function(tree){
+        if (tree.dataset.id === id){
+            bmTree = tree;
+        }
+        bmHide(tree);
+    });
+
+    if (bmTree){
+        bmShow(bmTree);
+        parentId = bmTree.dataset.parentid;
+    }else{
+        bmTree = document.createElement('div');
+        bmTree.classList.add('bm-tree');
+        bmTree.dataset.id = id;
+        bmTree.dataset.parentid = parentId;
+        
+        chrome.bookmarks.getChildren(id, function(children){
+            children.forEach(function(child) {
+                var bmItem;
+                if (!child.url){
+                    bmItem = createBmItem(child.title, '#', '../res/font-awesome/folder-regular.svg', child.id, id);
+                }else{
+                    bmItem = createBmItem(child.title, child.url, 'chrome://favicon/' + child.url);
+                }
+                if (bmItem){
+                    bmTree.appendChild(bmItem);
+                }
+            });
+        });
+        bmShow(bmTree);
+        document.body.appendChild(bmTree);
+    }
+
+    var bmUpper = document.getElementById('bm-upper');
+    if (parentId === '0'){
+        bmHide(bmUpper);
+    }else{
+        bmUpper.dataset.id = parentId;
+        bmShow(bmUpper);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var bmUpperLink = document.querySelector('#bm-upper a');
+    bmUpperLink.innerText = chrome.i18n.getMessage("upper");
+
+    // ref: https://www.w3schools.com/howto/howto_js_sticky_header.asp
+    var bmUpper = document.getElementById('bm-upper');
+    var sticky = bmUpper.offsetTop;
+    window.onscroll = function() {
+        if (window.pageYOffset > sticky) {
+            bmUpper.classList.add("bm-sticky");
+        } else {
+            bmUpper.classList.remove("bm-sticky");
+        }
+    };
+
+    bmUpper.onclick = function(ev){
+        var upper = ev.currentTarget;
+        loadFolder(upper.dataset.id);
+    };
+    
+    loadFolder('1', '0');
+});
+
+
+
