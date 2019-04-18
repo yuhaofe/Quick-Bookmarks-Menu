8d823b250263e264ece493df98f53e71997d433a
Yuhao Feng <git@yuhaofe.com>
2019-04-19 04:19:55 +0800

    many changes for 0.3.0 use es6. add auto enter folder on mouse hover. add search box. add open in... custom. add startup folder custom. compress icons.

--- src/background.js
+++ src/background.js
@@ -1,39 +1,131 @@
-chrome.storage.local.get(['startup'], function(result){
-    var barChecked = true;
-    if (result.startup && result.startup === '2'){
-        barChecked = false;
+chrome.storage.local.get(['openIn', 'hoverEnter', 'startup'], ({ openIn, hoverEnter, startup }) => {
+    const qbm = {
+        startup: '1',
+        openIn: 'new',
+        hoverEnter: 'medium'
+    };
+
+    if (!startup) {
+        chrome.storage.local.set({ startup } = qbm);
+    } else {
+        qbm.startup = startup;
     }
 
-    chrome.bookmarks.get(['1'], function (nodes) {
-        chrome.contextMenus.create({
-            id: 'startup_bar',
-            title: nodes[0].title,
-            type: 'radio',
-            checked: barChecked,
-            contexts: ['browser_action']
-        });
+    if (!openIn) {
+        chrome.storage.local.set({ openIn } = qbm);
+    } else {
+        qbm.openIn = openIn;
+    }
+
+    if (!hoverEnter) {
+        chrome.storage.local.set({ hoverEnter } = qbm);
+    } else {
+        qbm.hoverEnter = hoverEnter;
+    }
+
+    // open in menus
+    const openInChecked = {
+        new: [true, false, false],
+        current: [false, true, false],
+        background: [false, false, true]
+    }
+    chrome.contextMenus.create({
+        id: 'open_in',
+        title: chrome.i18n.getMessage("open_in"),
+        type: 'normal',
+        contexts: ['browser_action']
+    });
+    chrome.contextMenus.create({
+        id: 'open_in_new',
+        title: chrome.i18n.getMessage("open_in_new"),
+        type: 'radio',
+        checked: openInChecked[qbm.openIn][0],
+        parentId: 'open_in',
+        contexts: ['browser_action']
+    });
+    chrome.contextMenus.create({
+        id: 'open_in_current',
+        title: chrome.i18n.getMessage("open_in_current"),
+        type: 'radio',
+        checked: openInChecked[qbm.openIn][1],
+        parentId: 'open_in',
+        contexts: ['browser_action']
+    });
+    chrome.contextMenus.create({
+        id: 'open_in_background',
+        title: chrome.i18n.getMessage("open_in_background"),
+        type: 'radio',
+        checked: openInChecked[qbm.openIn][2],
+        parentId: 'open_in',
+        contexts: ['browser_action']
     });
 
-    chrome.bookmarks.get(['2'], function (nodes) {   
-        chrome.contextMenus.create({
-            id: 'startup_other',
-            title: nodes[0].title,
-            type: 'radio',
-            checked: !barChecked,
-            contexts: ['browser_action']
-        });
+    // hover enter menus
+    const hoverEnterChecked = {
+        off: [true, false, false, false],
+        slow: [false, true, false, false],
+        medium: [false, false, true, false],
+        fast: [false, false, false, true]
+    }
+    chrome.contextMenus.create({
+        id: 'hover_enter',
+        title: chrome.i18n.getMessage("hover_enter"),
+        type: 'normal',
+        contexts: ['browser_action']
     });
+    chrome.contextMenus.create({
+        id: 'hover_enter_off',
+        title: chrome.i18n.getMessage("hover_enter_off"),
+        type: 'radio',
+        checked: hoverEnterChecked[qbm.hoverEnter][0],
+        parentId: 'hover_enter',
+        contexts: ['browser_action']
+    });
+    chrome.contextMenus.create({
+        id: 'hover_enter_slow',
+        title: chrome.i18n.getMessage("hover_enter_slow"),
+        type: 'radio',
+        checked: hoverEnterChecked[qbm.hoverEnter][1],
+        parentId: 'hover_enter',
+        contexts: ['browser_action']
+    });
+    chrome.contextMenus.create({
+        id: 'hover_enter_medium',
+        title: chrome.i18n.getMessage("hover_enter_medium"),
+        type: 'radio',
+        checked: hoverEnterChecked[qbm.hoverEnter][2],
+        parentId: 'hover_enter',
+        contexts: ['browser_action']
+    });
+    chrome.contextMenus.create({
+        id: 'hover_enter_fast',
+        title: chrome.i18n.getMessage("hover_enter_fast"),
+        type: 'radio',
+        checked: hoverEnterChecked[qbm.hoverEnter][3],
+        parentId: 'hover_enter',
+        contexts: ['browser_action']
+    });
+
 });
 
-chrome.contextMenus.onClicked.addListener(function (info){
-    switch (info.menuItemId) {
-        case 'startup_bar':
-            chrome.storage.local.set({ startup: '1' });
-            break;
-        case 'startup_other':
-            chrome.storage.local.set({ startup: '2' });
-            break;
-        default:
-            break;
-    }
+chrome.contextMenus.onClicked.addListener(({ menuItemId, parentMenuItemId }) => {
+    const open_in = {
+        key: 'openIn',
+        open_in_new: 'new',
+        open_in_current: 'current',
+        open_in_background: 'background'
+    };
+    const hover_enter = {
+        key: 'hoverEnter',
+        hover_enter_off: 'off',
+        hover_enter_slow: 'slow',
+        hover_enter_medium: 'medium',
+        hover_enter_fast: 'fast'
+    };
+    const menus = {
+        open_in,
+        hover_enter
+    };
+    const parentMenu = menus[parentMenuItemId];
+    chrome.storage.local.set({ [parentMenu.key]: parentMenu[menuItemId] });
 });
