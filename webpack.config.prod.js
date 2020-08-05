da559d6125556ed547333da0ca161a9899d01f2d
Yuhao Feng <flyhaozi@live.com>
2020-08-05 09:22:03 +0800

    Remove webpack zip plugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,12 +1,6 @@
 const { merge } = require('webpack-merge');
-const ZipPlugin = require('zip-webpack-plugin');
 const common = require('./webpack.config.common');
 
 module.exports = merge(common, {
   mode: 'production',
-  plugins: [
-    new ZipPlugin({
-      filename: 'dist.zip',
-    }),
-  ],
 });
